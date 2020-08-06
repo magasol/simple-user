3bc4f01406bbcb7c3e93cb08f201ecf9adc2c242
magasol <magdalena.j.solecka@gmail.com>
2020-08-06 11:02:58 +0200

    expanding login data in person model, catching no empty db exception, added querying for avg age, most common cities and psw

--- simple-users/person_service.py
+++ simple-users/person_service.py
@@ -2,7 +2,7 @@ import json
 from datetime import datetime
 
 import requests
-from peewee import OperationalError, fn
+from peewee import fn, SQL
 
 from models import Person
 
@@ -17,17 +17,37 @@ class PersonService:
         Person.drop_table()
 
     def get_persons(self, count):
-        try:
-            query = Person.select().limit(count)
-            return [person.name_first + ' ' + person.name_last for person in query]
-        except OperationalError:
-            print('Database is empty please download some persons first')
+        results = Person.select().limit(count)
+        return [person.name_first + ' ' + person.name_last
+                + ' ' + person.gender + ' ' + str(person.dob_age) for person in results]
 
     def get_gender_percentage(self, gender):
-        gender_query = Person.select(fn.COUNT(Person.gender).alias('count')).where(Person.gender == gender).get()
-        persons_query = Person.select(fn.COUNT(Person.gender).alias('count')).get()
-        gender_percentage = gender_query.count / persons_query.count * 100
+        gender_res = Person.select(fn.COUNT(Person.gender).alias('count')) \
+            .where(Person.gender == gender).get()
+        persons_res = Person.select(fn.COUNT(Person.gender).alias('count')).get()
+        gender_percentage = gender_res.count / persons_res.count * 100
         return gender_percentage
+
+    def get_avg_age(self, gender):
+        if gender == 'all':
+            res = Person.select(fn.AVG(Person.dob_age).alias('avg_age')).get()
+            return res.avg_age
+        else:
+            res = Person.select(fn.AVG(Person.dob_age).alias('avg_age')) \
+                .where(Person.gender == gender).get()
+            return res.avg_age
+
+    def most_common(self, col, n):
+        if col == 'cities':
+            col_name_alias = 'city_name'
+            col_name = Person.location_city
+        else:
+            col_name_alias = 'pswd'
+            col_name = Person.login_password
+
+        results = Person.select(col_name.alias(col_name_alias), fn.COUNT(col_name).alias('occur')) \
+            .group_by(col_name).order_by(SQL('occur').desc()).limit(n)
+        return results
 
     def insert_persons(self, persons):
         print('Inserting persons into db')
@@ -51,7 +71,13 @@ class PersonService:
             location_coordinates=person['location']['coordinates'],
             location_timezone=person['location']['timezone'],
             email=person['email'],
-            login=person['login'],
+            login_uuid=person['login']['uuid'],
+            login_username=person['login']['username'],
+            login_password=person['login']['password'],
+            login_salt=person['login']['salt'],
+            login_md5=person['login']['md5'],
+            login_sha1=person['login']['sha1'],
+            login_sha256=person['login']['sha256'],
             dob_date=person['dob']['date'],
             dob_age=person['dob']['age'],
             registered_date=person['registered']['date'],
--- simple-users/script.py
+++ simple-users/script.py
@@ -1,6 +1,9 @@
 import click
+from peewee import OperationalError
 
 from person_service import PersonService
+
+ps = PersonService()
 
 
 @click.group()
@@ -8,40 +11,73 @@ def commands():
     pass
 
 
-@commands.command(name='info', help="Test if commands are working")
+@commands.command(name='info', help="About program")
 def info():
-    click.echo("A program about random persons")
+    click.echo("A program about randomly generated persons data")
 
 
 @commands.command(name='clear', help="Clear entire database")
 def clear_db():
     print("Clearing db")
-    ps = PersonService()
     ps.drop_persons()
 
 
 @commands.command(name='load', help="Load persons into database")
-@click.option('--count', type=int, default=20, help="Number of new persons loaded")
+@click.argument('count', type=int, default=20)
 def load_persons(count):
     print("Downloading")
-    ps = PersonService()
     persons = ps.load_persons(count)
     ps.insert_persons(persons)
 
 
 @commands.command(name='list', help="List persons")
-@click.option('--count', help="List all persons")
+@click.argument('count', type=int, default=10)
 def list_persons(count):
     print("Raading persons")
-    ps = PersonService()
-    print(ps.get_persons(count))
+    try:
+        print(ps.get_persons(count))
+    except OperationalError:
+        print('Database is empty please download some persons first')
 
 
-@commands.command(name='-gender-percentage', help="Percentage of men or women")
+@commands.command(name='gender-percentage', help="Count percentage")
 @click.argument('gender', type=click.Choice(['male', 'female']))
 def gender_percentage(gender):
-    ps = PersonService()
-    print(ps.get_gender_percentage(gender))
+    try:
+        print("{}: {}%".format(gender, ps.get_gender_percentage(gender)))
+    except OperationalError:
+        print('Database is empty please download some persons first')
+
+
+@commands.command(name='average-age', help="Count average age of men or women or general (by default)")
+@click.argument('gender', default='all', type=click.Choice(['male', 'female', 'all']))
+def gender_avg_age(gender):
+    try:
+        print("{}: {}".format(gender, ps.get_avg_age(gender)))
+    except OperationalError:
+        print('Database is empty please download some persons first')
+
+
+@commands.command(name='most-common-cities', help="Show most common cities and number of its occurrences")
+@click.argument('n', default='1', type=int)
+def most_common_cities(n):
+    try:
+        results = ps.most_common('cities', n)
+        for r in results:
+            print("{}, {}".format(r.city_name, r.occur))
+    except OperationalError:
+        print('Database is empty please download some persons first')
+
+
+@commands.command(name='most-common-passwords', help="Show most common passwords and number of its occurrences")
+@click.argument('n', default='1', type=int)
+def most_common_passwords(n):
+    try:
+        results = ps.most_common('passwords', n)
+        for r in results:
+            print("{}, {}".format(r.pswd, r.occur))
+    except OperationalError:
+        print('Database is empty please download some persons first')
 
 
 if __name__ == '__main__':
